b152f1202af64e8f41d0fdf21fc0d5c38fa0ca64
[Ethan] <[nesbitt-e2@email.ulster.ac.uk]>
2018-02-09 22:33:50 +0000

    Added comments to numerous pages and methods for conversions added, conversion activity also designed

--- app/src/main/java/com/example/ethannesbitt/youcook/Converter.java
+++ app/src/main/java/com/example/ethannesbitt/youcook/Converter.java
@@ -31,6 +31,7 @@ public class Converter extends AppCompatActivity implements NavigationView.OnNav
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
 
+        //initialising drawer menu
         drawerMenu = (DrawerLayout) findViewById(R.id.converter);
         menuToggle = new ActionBarDrawerToggle(this, drawerMenu, R.string.open, R.string.close);
         drawerMenu.addDrawerListener(menuToggle);
@@ -40,6 +41,7 @@ public class Converter extends AppCompatActivity implements NavigationView.OnNav
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    //allows drawer menu to be opened via a button in title bar
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
@@ -50,6 +52,7 @@ public class Converter extends AppCompatActivity implements NavigationView.OnNav
         return super.onOptionsItemSelected(item);
     }
 
+    //drawer menu navigation, on clicks for each item in the menu, finishes current activity and starts the new activity
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item)
     {
@@ -97,6 +100,136 @@ public class Converter extends AppCompatActivity implements NavigationView.OnNav
         return false;
     }
 
+
+    //conversion methods 'to Grams'
+    private String kiloToGram (double k)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String poundToGram (double p)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String ounceToGram (double o)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String cupToGram (double c)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+
+    //conversion methods 'to Kilos'
+    private String gramToKilo (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String poundToKilo (double p)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String ounceToKilo (double o)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String cupToKilo (double c)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+
+    //conversion methods 'to Pounds'
+    private String kiloToPound (double k)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String gramToPound (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String ounceToPound (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String cupToPound (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+
+    //conversion methods 'to Ounces'
+    private String kiloToOunce (double k)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String gramToOunce (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String poundToPound (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String cupToPound (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    //conversion methods 'to Cups'
+    private String kiloToCup (double k)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String gramToCup (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String poundToCup (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    private String ounceToCup (double g)
+    {
+        String finalResult = ;
+        return finalResult;
+    }
+
+    //sign out method to allow user to sign out of account/app
     private void signOut()
     {
         mAuth.signOut();
