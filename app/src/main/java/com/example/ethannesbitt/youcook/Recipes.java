07521ea178483bd5af4dd0dc139bd776a89565bd
[Ethan] <[nesbitt-e2@email.ulster.ac.uk]>
2018-03-19 12:16:56 +0000

    Some design changes to the recipe page and information tab, changed button layout and added more space to allow for easier reading for the user.

--- app/src/main/java/com/example/ethannesbitt/youcook/Recipes.java
+++ app/src/main/java/com/example/ethannesbitt/youcook/Recipes.java
@@ -7,19 +7,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 
 public class Recipes extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
